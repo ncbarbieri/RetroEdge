9b63702dc65e7beb7f7ff3bea5e2e4507b2a6c96
ncbarbieri <nbarbieri@itisavogadro.it>
2025-03-31 16:49:32 +0200

    Comments added

--- src/pathfinder/ObstacleInflationUtil.java
+++ src/pathfinder/ObstacleInflationUtil.java
@@ -1,3 +1,10 @@
+/**
+ * RetroEdge Educational Game Engine
+ * 
+ * Copyright (c) 2025 Nicola Christian Barbieri
+ * Licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
+ */
 package pathfinder;
 
 public class ObstacleInflationUtil {
